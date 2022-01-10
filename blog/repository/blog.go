0a95c583d3dd4279a2cc965bfb2d8748520e2024
Grecz Balázs <greczb20@gmail.com>
2022-01-10 12:40:32 +0100

    reset

--- blog/repository/blog.go
+++ blog/repository/blog.go
@@ -1,19 +1,14 @@
 package blogRespository
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Blog struct {
 	gorm.Model
-	ID        int64     `gorm:"primary_key;auto_increment" json:"id"`
-	Title     string    `gorm:"size:200" json:"title"`
-	Body      string    `gorm:"size:3000" json:"body" `
-	CreatedAt time.Time `json:"created_at,omitempty"`
-	UpdatedAt time.Time `json:"updated_at,omitempty"`
-	db        *gorm.DB
+	Title string `json:"title"`
+	Body  string `json:"body"`
+	db    *gorm.DB
 }
 
 func New(db *gorm.DB) *Blog {
@@ -25,24 +20,10 @@ func New(db *gorm.DB) *Blog {
 func (b *Blog) Save(post Blog) error {
 	return b.db.DB.Create(&post).Error
 }
-func (b *Blog) FindAll(post Blog, keyword string) (*[]Blog, int64, error) {
-	var posts []Blog
-	var totalRows int64 = 0
-
-	queryBuider := b.db.DB.Order("created_at desc").Model(&Blog{})
-
-	// Search parameter
-	if keyword != "" {
-		queryKeyword := "%" + keyword + "%"
-		queryBuider = queryBuider.Where(
-			b.db.DB.Where("post.title LIKE ? ", queryKeyword))
-	}
-
-	err := queryBuider.
-		Where(post).
-		Find(&posts).
-		Count(&totalRows).Error
-	return &posts, totalRows, err
+func (b *Blog) FindAll(post Blog, keyword string) {
+	blog := make([]Blog, 0)
+	b.db.Find(&blog)
+	c.JSON(blog)
 }
 func (b *Blog) Update(post Blog) error {
 	return b.db.DB.Save(&post).Error
--- blog/usecase/blog.go
+++ blog/usecase/blog.go
@@ -1,35 +1,13 @@
 package blogUsecase
 
-import (
-	"github.com/Higins/go_blog2/*"
-)
-
-type blogRespository struct {
+type blogUsecase struct {
 	service blogRespository.Blog
 }
 
-func NewBlogController(s blogRespository.Blog) blogRespository {
-	return blogRespository{
-		service: s,
-	}
+func NewBlogController(s blogRespository.Blog) {
+
 }
 
 func (p *blogRespository) AddNewBlog(ctx *gin.Context) {
-    var post models.Post
-    ctx.ShouldBindJSON(&post)
 
-    if post.Title == "" {
-        util.ErrorJSON(ctx, http.StatusBadRequest, "Title is required")
-        return
-    }
-    if post.Body == "" {
-        util.ErrorJSON(ctx, http.StatusBadRequest, "Body is required")
-        return
-    }
-    err := p.service.Save(post)
-    if err != nil {
-        util.ErrorJSON(ctx, http.StatusBadRequest, "Failed to create post")
-        return
-    }
-    util.SuccessJSON(ctx, http.StatusCreated, "Successfully Created Post")
 }
